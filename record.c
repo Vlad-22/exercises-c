e02087008d0932717b8f92f0e0b288744d9afca7
Clince Vlad-Alexandru <100097421+VladClince@users.noreply.github.com>
2022-12-26 23:05:31 +0200

    Update record.c

--- record.c
+++ record.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
+#include <math.h>
 
+int main(void) {
+    double r = 2;
+    scanf("%lf", &r);
+    printf("Aria = %.2lf Parametrul = %.2lf\n", M_PI*r*r, 2*M_PI*r);
+    return 0;
+}
+
+/* gcc record.c
+Aria = 12.57 Parametrul = 12.57
+*/ 
